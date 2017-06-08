624c7a90f593ca0847e9c2dbfa51de65f220ff2a
Rafael Borges Machado <rafaeobm@hotmail.com>
2017-06-08 01:43:20 -0300

    Add FindTests Performance tests related to various ways to find elements in a collection

---
+++ PerfBenchmarks/Find.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace PerfBenchmarks
+{
+    [Config(typeof(BenchConfig))]
+    public class FindTests
+    {
+        private readonly List<string> _items = Enumerable.Range(0, 50000).Select(i => "Index" + i).ToList();
+
+        [Benchmark]
+        public void Find()
+        {
+            var i = _items.Find(s => s == "Index700");
+        }
+
+        [Benchmark]
+        public void FirstOrDefault()
+        {
+            var i = _items.FirstOrDefault(s => s == "Index700");
+        }
+
+        [Benchmark]
+        public void WhereThenFirstOrDefault()
+        {
+            var i = _items.Where(s => s == "Index700").FirstOrDefault();
+        }
+    }
+}
