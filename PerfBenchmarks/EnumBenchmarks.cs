01519084d7ba6f2cff24c81f026660775602409f
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2021-02-25 00:38:51 -0300

    Add EnumBenchmarks
    Benchmarking several ways to compare as string to Enum names

---
+++ PerfBenchmarks/EnumBenchmarks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace PerfBenchmarks
+{
+    [Config(typeof(BenchConfig))]
+    public class EnumBenchmarks
+    {
+        private string _myEnumValueAsString;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _myEnumValueAsString = "Value1";
+        }
+
+
+        [Benchmark(Baseline = true)]
+        public bool EqualityCheckCastingEnumToString()
+        {
+            return _myEnumValueAsString == MyEnum.Value1.ToString();
+        }
+
+        [Benchmark]
+        public bool EqualityCheckIsDefined()
+        {
+            return Enum.IsDefined(typeof(MyEnum), _myEnumValueAsString);
+        }
+
+        [Benchmark]
+        public bool EqualityCheckReturningTrueTryParse()
+        {
+            return Enum.TryParse(_myEnumValueAsString, out MyEnum _);
+        }
+    }
+
+    public enum MyEnum
+    {
+        Value0,
+        Value1,
+    }
+}
