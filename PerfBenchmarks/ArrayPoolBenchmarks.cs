e47f6533596ea45ac73b3abb994b69518e6df0e1
rafaelbm <5865557+rafaelbm@users.noreply.github.com>
2021-10-24 20:08:31 -0300

    Add ArrayPoolBenchmarks

---
+++ PerfBenchmarks/ArrayPoolBenchmarks.cs
@@ -0,0 +1,63 @@
+﻿using System.Buffers;
+using BenchmarkDotNet.Attributes;
+
+namespace PerfBenchmarks
+{
+    [MemoryDiagnoser]
+    public class ArrayPoolBenchmarks
+    {
+        private Processor _processor;
+
+        [Params(20, 100, 1_000, 10_000, 100_000)]
+        public int Size { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _processor = new Processor();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Original()
+        {
+            _processor.DoSomeWorkVeryOften(Size);
+        }
+
+        [Benchmark]
+        public void UsingArrayPool()
+        {
+            _processor.DoSomeWorkVeryOftenUsingArrayPool(Size);
+        }
+    }
+
+    public class Processor
+    {
+        public void DoSomeWorkVeryOften(int size)
+        {
+            var buffer = new byte[size];
+
+            DoSomethingWithBuffer(buffer);
+        }
+
+        public void DoSomeWorkVeryOftenUsingArrayPool(int size)
+        {
+            var arrayPool = ArrayPool<byte>.Shared;
+            var buffer = arrayPool.Rent(size);
+
+            try
+            {
+                DoSomethingWithBuffer(buffer);
+
+            }
+            finally
+            {
+                arrayPool.Return(buffer);
+            }
+        }
+
+        private void DoSomethingWithBuffer(byte[] buffer)
+        {
+            //  use the array
+        }
+    }
+}
